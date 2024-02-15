4c0a0337ab8e3b6cf1a4634ec9eb4d5bd6f79e84
Jiuh.star <jiuh.star@gmail.com>
2024-02-15 12:46:05 +0800

    small refactor for more clear code

--- src/chatserver/__init__.py
+++ src/chatserver/__init__.py
@@ -1,15 +1,13 @@
 import asyncio
+
+from typing import NoReturn
 from quart import Quart, render_template, websocket
+
 from .broker import Broker
 
 app = Quart(__name__)
 broker = Broker()
 
-
-async def _receive():
-    while True:
-        message = await websocket.receive()
-        await broker.publish(message)
 
 def run():
     app.run()
@@ -20,13 +18,20 @@ async def index():
     return await render_template("index.html")
 
 
+async def _send() -> NoReturn:
+    async for message in broker.subscribe():
+        await websocket.send(message)
+
+
+async def _receive() -> NoReturn:
+    while True:
+        message = await websocket.receive()
+        await broker.publish(message)
+
+
 @app.websocket("/ws")
 async def ws():
-    task = asyncio.create_task(_receive())
+    producer = asyncio.create_task(_send())
+    consumer = asyncio.create_task(_receive())
 
-    try:
-        async for message in broker.subscribe():
-            await websocket.send(message)
-    finally:
-        task.cancel()
-        await task
+    await asyncio.gather(producer, consumer)
